91e97956462f2af7ed4c3de8b6b77b6d1711ade3
Ryan Loh <growwick@gmail.com>
2024-05-08 23:35:52 +0800

    update ping mechanism

--- src/main.c
+++ src/main.c
@@ -222,7 +222,7 @@ int main(int argc, char const *argv[]) {
 	/* code */
 	srand(time(NULL));
 
-	printf("chipvpn 1.7.3\n"); 
+	printf("chipvpn 1.7.3 rc1\n"); 
 
 	if(!(argc > 1 && argv[1] != NULL)) {
 		printf("config path required\n");
