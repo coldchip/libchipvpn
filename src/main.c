4d6e67cece40cb0b553b0672716dbf850cbb72c7
Ryan Loh <growwick@gmail.com>
2024-05-11 06:02:00 +0800

    remove libsodium

--- src/main.c
+++ src/main.c
@@ -222,7 +222,7 @@ int main(int argc, char const *argv[]) {
 	/* code */
 	srand(time(NULL));
 
-	printf("chipvpn 1.7.3 rc5\n"); 
+	printf("chipvpn 1.7.3 rc6\n"); 
 
 	if(!(argc > 1 && argv[1] != NULL)) {
 		printf("config path required\n");
