2d0f2f78d18e0e544aa8bab6474ca3c510fbf2fa
Ryan Loh <growwick@gmail.com>
2025-03-24 21:16:02 +0800

    update

--- hmac.c
+++
@@ -1,9 +0,0 @@
-#include <stddef.h>
-
-extern size_t hmac_sha256(const void* key, const size_t keylen, const void* data, void* out, const size_t outlen);
-
-size_t hmac_sha256(const void* key, const size_t keylen, const void* data, void* out, const size_t outlen) {
-
-	printf("hello\n");
-
-}
