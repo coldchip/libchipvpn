c20c31bfcbbd704d77fe8a84b7d731c7df80f3ea
Ryan Loh <growwick@gmail.com>
2025-06-24 22:57:45 +0800

    update

--- src/crypto.c
+++ src/crypto.c
@@ -20,13 +20,13 @@ void chipvpn_crypto_poly1305_init(chipvpn_crypto_t *crypto) {
 
 	char key2[32] = {0};
 	char nonce2[12] = {0};
-	xchacha_hchacha20(key2, (unsigned char*)crypto->nonce, (unsigned char*)crypto->key);
+	xchacha_hchacha20((uint8_t*)key2, (uint8_t*)crypto->nonce, (uint8_t*)crypto->key);
 
 
 	memcpy(nonce2 + 4, crypto->nonce + 16, 12 - 4);
 
 	struct chacha20_context ctx;
-	chacha20_init_context(&ctx, key2, nonce2, 0);
+	chacha20_init_context(&ctx, (uint8_t*)key2, (uint8_t*)nonce2, 0);
 	chacha20_xor(&ctx, (uint8_t*)&crypto->block0, sizeof(crypto->block0));
 }
 
@@ -43,16 +43,39 @@ void chipvpn_crypto_xchacha20(chipvpn_crypto_t *crypto, void *data, int size, ui
 	
 }
 
+static const unsigned char _pad0[16] = { 0 };
+
 void chipvpn_crypto_xchacha20_poly1305_encrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
 	chipvpn_crypto_xchacha20(crypto, data, size, counter);
 
-	poly1305_auth((unsigned char*)mac, data, size, (unsigned char*)&crypto->block0);
+	poly1305_context ctx;
+	poly1305_init(&ctx, (unsigned char*)&crypto->block0);
+	poly1305_update(&ctx, (unsigned char*)data, size);
+	poly1305_update(&ctx, (unsigned char*)_pad0, (0x10 - size) & 0xf);
+
+	uint64_t aad_size = 0l;
+	uint64_t data_size = size;
+
+	poly1305_update(&ctx, (unsigned char*)&aad_size, sizeof(aad_size));
+	poly1305_update(&ctx, (unsigned char*)&data_size, sizeof(data_size));
+
+	poly1305_finish(&ctx, (unsigned char*)mac);
 
 }
 
 void chipvpn_crypto_xchacha20_poly1305_decrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
+	poly1305_context ctx;
+	poly1305_init(&ctx, (unsigned char*)&crypto->block0);
+	poly1305_update(&ctx, (unsigned char*)data, size);
+	poly1305_update(&ctx, (unsigned char*)_pad0, (0x10 - size) & 0xf);
 
-	poly1305_auth((unsigned char*)mac, data, size, (unsigned char*)&crypto->block0);
+	uint64_t aad_size = 0l;
+	uint64_t data_size = size;
+
+	poly1305_update(&ctx, (unsigned char*)&aad_size, sizeof(aad_size));
+	poly1305_update(&ctx, (unsigned char*)&data_size, sizeof(data_size));
+
+	poly1305_finish(&ctx, (unsigned char*)mac);
 
 	chipvpn_crypto_xchacha20(crypto, data, size, counter);
 }
