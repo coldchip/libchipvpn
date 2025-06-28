6043386297b7308b9a798a17ebe6893278624d96
Ryan Loh <growwick@gmail.com>
2025-06-29 00:24:33 +0800

    update

--- src/chacha20.c
+++ src/chacha20.c
@@ -1,66 +1,32 @@
 #include "chacha20.h"
 
-static uint32_t rotl32(uint32_t x, int n) 
-{
-    return (x << n) | (x >> (32 - n));
-}
-
-static uint32_t pack4(const uint8_t *a)
-{
-    uint32_t res = 0;
-    res |= (uint32_t)a[0] << 0 * 8;
-    res |= (uint32_t)a[1] << 1 * 8;
-    res |= (uint32_t)a[2] << 2 * 8;
-    res |= (uint32_t)a[3] << 3 * 8;
-    return res;
-}
-
-static void chacha20_init_block(struct chacha20_context *ctx, uint8_t key[], uint8_t nonce[])
-{
-    memcpy(ctx->key, key, sizeof(ctx->key));
-    memcpy(ctx->nonce, nonce, sizeof(ctx->nonce));
-
-    const uint8_t *magic_constant = (uint8_t*)"expand 32-byte k";
-    ctx->state[0] = pack4(magic_constant + 0 * 4);
-    ctx->state[1] = pack4(magic_constant + 1 * 4);
-    ctx->state[2] = pack4(magic_constant + 2 * 4);
-    ctx->state[3] = pack4(magic_constant + 3 * 4);
-    ctx->state[4] = pack4(key + 0 * 4);
-    ctx->state[5] = pack4(key + 1 * 4);
-    ctx->state[6] = pack4(key + 2 * 4);
-    ctx->state[7] = pack4(key + 3 * 4);
-    ctx->state[8] = pack4(key + 4 * 4);
-    ctx->state[9] = pack4(key + 5 * 4);
-    ctx->state[10] = pack4(key + 6 * 4);
-    ctx->state[11] = pack4(key + 7 * 4);
-    // 64 bit counter initialized to zero by default.
+static void chacha20_init_block(struct chacha20_context *ctx, uint8_t key[], uint8_t nonce[]) {
+    ctx->state[0]  = 0x61707865;
+    ctx->state[1]  = 0x3320646e;
+    ctx->state[2]  = 0x79622d32;
+    ctx->state[3]  = 0x6b206574;
+    ctx->state[4]  = PACK4_LE(key + 0 * 4);
+    ctx->state[5]  = PACK4_LE(key + 1 * 4);
+    ctx->state[6]  = PACK4_LE(key + 2 * 4);
+    ctx->state[7]  = PACK4_LE(key + 3 * 4);
+    ctx->state[8]  = PACK4_LE(key + 4 * 4);
+    ctx->state[9]  = PACK4_LE(key + 5 * 4);
+    ctx->state[10] = PACK4_LE(key + 6 * 4);
+    ctx->state[11] = PACK4_LE(key + 7 * 4);
     ctx->state[12] = 0;
-    ctx->state[13] = pack4(nonce + 0 * 4);
-    ctx->state[14] = pack4(nonce + 1 * 4);
-    ctx->state[15] = pack4(nonce + 2 * 4);
-
-    memcpy(ctx->nonce, nonce, sizeof(ctx->nonce));
+    ctx->state[13] = PACK4_LE(nonce + 0 * 4);
+    ctx->state[14] = PACK4_LE(nonce + 1 * 4);
+    ctx->state[15] = PACK4_LE(nonce + 2 * 4);
 }
 
-static void chacha20_block_set_counter(struct chacha20_context *ctx, uint64_t counter)
-{
+static void chacha20_block_set_counter(struct chacha20_context *ctx, uint32_t counter) {
     ctx->state[12] = (uint32_t)counter;
-    ctx->state[13] = pack4(ctx->nonce + 0 * 4) + (uint32_t)(counter >> 32);
 }
 
 static void chacha20_block_next(struct chacha20_context *ctx) {
-    // This is where the crazy voodoo magic happens.
-    // Mix the bytes a lot and hope that nobody finds out how to undo it.
-    for (int i = 0; i < 16; i++) ctx->keystream32[i] = ctx->state[i];
+    for(int i = 0; i < 16; i++) ctx->keystream32[i] = ctx->state[i];
 
-#define CHACHA20_QUARTERROUND(x, a, b, c, d) \
-    x[a] += x[b]; x[d] = rotl32(x[d] ^ x[a], 16); \
-    x[c] += x[d]; x[b] = rotl32(x[b] ^ x[c], 12); \
-    x[a] += x[b]; x[d] = rotl32(x[d] ^ x[a], 8); \
-    x[c] += x[d]; x[b] = rotl32(x[b] ^ x[c], 7);
-
-    for (int i = 0; i < 10; i++) 
-    {
+    for(int i = 0; i < 10; i++)  {
         CHACHA20_QUARTERROUND(ctx->keystream32, 0, 4, 8, 12)
         CHACHA20_QUARTERROUND(ctx->keystream32, 1, 5, 9, 13)
         CHACHA20_QUARTERROUND(ctx->keystream32, 2, 6, 10, 14)
@@ -71,42 +37,24 @@ static void chacha20_block_next(struct chacha20_context *ctx) {
         CHACHA20_QUARTERROUND(ctx->keystream32, 3, 4, 9, 14)
     }
 
-    for (int i = 0; i < 16; i++) ctx->keystream32[i] += ctx->state[i];
+    for(int i = 0; i < 16; i++) ctx->keystream32[i] += ctx->state[i];
 
-    uint32_t *counter = ctx->state + 12;
-    // increment counter
-    counter[0]++;
-    if (0 == counter[0]) 
-    {
-        // wrap around occured, increment higher 32 bits of counter
-        counter[1]++;
-        // Limited to 2^64 blocks of 64 bytes each.
-        // If you want to process more than 1180591620717411303424 bytes
-        // you have other problems.
-        // We could keep counting with counter[2] and counter[3] (nonce),
-        // but then we risk reusing the nonce which is very bad.
-        assert(0 != counter[1]);
-    }
+    ctx->state[12] += 1;
 }
 
-void chacha20_init_context(struct chacha20_context *ctx, uint8_t key[], uint8_t nonce[], uint64_t counter)
-{
+void chacha20_init_context(struct chacha20_context *ctx, uint8_t key[], uint8_t nonce[], uint32_t counter) {
     memset(ctx, 0, sizeof(struct chacha20_context));
 
     chacha20_init_block(ctx, key, nonce);
     chacha20_block_set_counter(ctx, counter);
 
-    ctx->counter = counter;
     ctx->position = 64;
 }
 
-void chacha20_xor(struct chacha20_context *ctx, uint8_t *bytes, size_t n_bytes)
-{
+void chacha20_xor(struct chacha20_context *ctx, uint8_t *bytes, size_t n_bytes) {
     uint8_t *keystream8 = (uint8_t*)ctx->keystream32;
-    for (size_t i = 0; i < n_bytes; i++) 
-    {
-        if (ctx->position >= 64) 
-        {
+    for(size_t i = 0; i < n_bytes; i++) {
+        if(ctx->position >= 64) {
             chacha20_block_next(ctx);
             ctx->position = 0;
         }
--- src/chacha20.h
+++ src/chacha20.h
@@ -9,19 +9,33 @@
 extern "C" {
 #endif
 
-struct chacha20_context
-{
-  uint32_t keystream32[16];
-  size_t position;
+#define U8V(v) ((uint8_t)(v) & (0xFF))
+#define U16V(v) ((uint16_t)(v) & (0xFFFF))
+#define U32V(v) ((uint32_t)(v) & (0xFFFFFFFF))
+#define U64V(v) ((uint64_t)(v) & (0xFFFFFFFFFFFFFFFF))
 
-  uint8_t key[32];
-  uint8_t nonce[12];
-  uint64_t counter;
+#define PACK4_LE(p) \
+	(((uint32_t)((p)[0])     ) | \
+	((uint32_t)((p)[1]) <<  8) | \
+	((uint32_t)((p)[2]) << 16) | \
+	((uint32_t)((p)[3]) << 24))
 
-  uint32_t state[16];
+#define ROTL32(v, n) \
+	(U32V((v) << (n)) | ((v) >> (32 - (n))))
+
+#define CHACHA20_QUARTERROUND(x, a, b, c, d) \
+	x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 16); \
+	x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 12); \
+	x[a] += x[b]; x[d] = ROTL32(x[d] ^ x[a], 8); \
+	x[c] += x[d]; x[b] = ROTL32(x[b] ^ x[c], 7);
+
+struct chacha20_context {
+	uint32_t keystream32[16];
+	uint32_t state[16];
+	size_t position;
 };
 
-void chacha20_init_context(struct chacha20_context *ctx, uint8_t key[], uint8_t nounc[], uint64_t counter);
+void chacha20_init_context(struct chacha20_context *ctx, uint8_t key[], uint8_t nounc[], uint32_t counter);
 void chacha20_block_set_nonce(struct chacha20_context *ctx, uint8_t nonce[]);
 void chacha20_xor(struct chacha20_context *ctx, uint8_t *bytes, size_t n_bytes);
 
