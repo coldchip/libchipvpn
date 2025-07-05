d6514e5effd91d8714b7f1e6170eb41fc4323a17
Ryan Loh <growwick@gmail.com>
2025-07-05 21:10:37 +0800

    crypto refractor

--- src/chacha20poly1305.c
+++ src/chacha20poly1305.c
@@ -5,77 +5,101 @@
 #include "chacha20.h"
 #include "poly1305.h"
 
-void chipvpn_crypto_chacha20_poly1305_encrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
-	char nonce[12] = {0};
+bool chipvpn_crypto_chacha20_poly1305_encrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
+	struct chacha20_context chacha20_ctx;
+	poly1305_context poly1305_ctx;
+	char     nonce[12] = {0};
+	char     block0[64] = {0};
+	uint64_t aad_size = 0l;
+	uint64_t data_size = size;
+
+	// Create 96bit nonce from 64bit counter by copying to 32-96bit region 
 	memcpy(nonce + 4, &counter, sizeof(counter));
 
-	struct chacha20_context chacha20_ctx;
+	// Initialize chacha20 from key and nonce
 	chacha20_init_context(&chacha20_ctx, (uint8_t*)crypto->key, (uint8_t*)nonce, 0);
 
-	char block0[64] = {0};
+	// Generate poly1305 key from key and nonce, internal counter = 0
 	chacha20_xor(&chacha20_ctx, (uint8_t*)block0, sizeof(block0));
 
+	// Encrypt payload, internal counter = 1..2.. and so on
 	chacha20_xor(&chacha20_ctx, (uint8_t*)data, size);
 
-	poly1305_context poly1305_ctx;
+	// Update poly1305 with data and padding
 	poly1305_init(&poly1305_ctx, (unsigned char*)&block0);
 	poly1305_update(&poly1305_ctx, (unsigned char*)data, size);
 	poly1305_update(&poly1305_ctx, (unsigned char*)pad0, (0x10 - size) & 0xf);
 
-	uint64_t aad_size = 0l;
-	uint64_t data_size = size;
-
+	// Update poly1305 with aad(not needed) and size
 	poly1305_update(&poly1305_ctx, (unsigned char*)&aad_size, sizeof(aad_size));
 	poly1305_update(&poly1305_ctx, (unsigned char*)&data_size, sizeof(data_size));
 
+	// Compute poly1305 and output to mac
 	poly1305_finish(&poly1305_ctx, (unsigned char*)mac);
+
+	return true;
 }
 
-void chipvpn_crypto_chacha20_poly1305_decrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
-	char nonce[12] = {0};
+bool chipvpn_crypto_chacha20_poly1305_decrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac) {
+	struct chacha20_context chacha20_ctx;
+	poly1305_context poly1305_ctx;
+	char     computed_mac[16] = {0};
+	char     nonce[12] = {0};
+	char     block0[64] = {0};
+	uint64_t aad_size = 0l;
+	uint64_t data_size = size;
+
+	// Create 96bit nonce from 64bit counter by copying to 32-96bit region 
 	memcpy(nonce + 4, &counter, sizeof(counter));
 
-	struct chacha20_context chacha20_ctx;
+	// Initialize chacha20 from key and nonce
 	chacha20_init_context(&chacha20_ctx, (uint8_t*)crypto->key, (uint8_t*)nonce, 0);
 
-	char block0[64] = {0};
+	// Generate poly1305 key from key and nonce, internal counter = 0
 	chacha20_xor(&chacha20_ctx, (uint8_t*)block0, sizeof(block0));
 
-	poly1305_context poly1305_ctx;
+	// Update poly1305 with data and padding
 	poly1305_init(&poly1305_ctx, (unsigned char*)&block0);
 	poly1305_update(&poly1305_ctx, (unsigned char*)data, size);
 	poly1305_update(&poly1305_ctx, (unsigned char*)pad0, (0x10 - size) & 0xf);
 
-	uint64_t aad_size = 0l;
-	uint64_t data_size = size;
-
+	// Update poly1305 with aad(not needed) and size
 	poly1305_update(&poly1305_ctx, (unsigned char*)&aad_size, sizeof(aad_size));
 	poly1305_update(&poly1305_ctx, (unsigned char*)&data_size, sizeof(data_size));
 
-	poly1305_finish(&poly1305_ctx, (unsigned char*)mac);
+	// Compute poly1305 and output to mac
+	poly1305_finish(&poly1305_ctx, (unsigned char*)computed_mac);
 
+	// Compare computed mac against input mac
+	if(chipvpn_crypto_memcmp16((uint8_t*)computed_mac, (uint8_t*)mac) != 0) {
+		return false;
+	}
+
+	// Finally, if mac matches, decrypt payload, internal counter = 1..2.. and so on
 	chacha20_xor(&chacha20_ctx, (uint8_t*)data, size);
+
+	return true;
 }
 
 int chipvpn_crypto_memcmp16(const uint8_t *a, const uint8_t *b) {
-    uint8_t diff = 0;
+	uint8_t diff = 0;
 
-    diff |= a[0] ^ b[0];
-    diff |= a[1] ^ b[1];
-    diff |= a[2] ^ b[2];
-    diff |= a[3] ^ b[3];
-    diff |= a[4] ^ b[4];
-    diff |= a[5] ^ b[5];
-    diff |= a[6] ^ b[6];
-    diff |= a[7] ^ b[7];
-    diff |= a[8] ^ b[8];
-    diff |= a[9] ^ b[9];
-    diff |= a[10] ^ b[10];
-    diff |= a[11] ^ b[11];
-    diff |= a[12] ^ b[12];
-    diff |= a[13] ^ b[13];
-    diff |= a[14] ^ b[14];
-    diff |= a[15] ^ b[15];
+	diff |= a[0] ^ b[0];
+	diff |= a[1] ^ b[1];
+	diff |= a[2] ^ b[2];
+	diff |= a[3] ^ b[3];
+	diff |= a[4] ^ b[4];
+	diff |= a[5] ^ b[5];
+	diff |= a[6] ^ b[6];
+	diff |= a[7] ^ b[7];
+	diff |= a[8] ^ b[8];
+	diff |= a[9] ^ b[9];
+	diff |= a[10] ^ b[10];
+	diff |= a[11] ^ b[11];
+	diff |= a[12] ^ b[12];
+	diff |= a[13] ^ b[13];
+	diff |= a[14] ^ b[14];
+	diff |= a[15] ^ b[15];
 
-    return diff;
+	return diff;
 }
--- src/chacha20poly1305.h
+++ src/chacha20poly1305.h
@@ -7,6 +7,7 @@ extern "C"
 #endif
 
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct {
 	char key[32];
@@ -14,8 +15,8 @@ typedef struct {
 
 static const unsigned char pad0[16] = { 0 };
 
-void                  chipvpn_crypto_chacha20_poly1305_encrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac);
-void                  chipvpn_crypto_chacha20_poly1305_decrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac);
+bool                  chipvpn_crypto_chacha20_poly1305_encrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac);
+bool                  chipvpn_crypto_chacha20_poly1305_decrypt(chipvpn_crypto_t *crypto, void *data, int size, uint64_t counter, char *mac);
 
 int                   chipvpn_crypto_memcmp16(const uint8_t *a, const uint8_t *b);
 
