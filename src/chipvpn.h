92e5fcf8c3a7104a30c1c2b80fd6aecdb9b54032
Ryan Loh <growwick@gmail.com>
2025-11-15 17:30:04 +0800

    restructure code

--- src/chipvpn.h
+++ src/chipvpn.h
@@ -15,7 +15,7 @@ extern "C"
 #include "udp.h"
 #include "ipc.h"
 
-#define CHIPVPN_VERSION 300006
+#define CHIPVPN_VERSION 300007
 #define CHIPVPN_PROTOCOL_VERSION 197
 
 #define CHIPVPN_PEER_TIMEOUT 15000
