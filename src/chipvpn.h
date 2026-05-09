188d2db5979d4b91a4bd2e07db3579b5119dc998
coldchip <growwick@gmail.com>
2026-05-09 20:40:06 +0800

    added mss option

--- src/chipvpn.h
+++ src/chipvpn.h
@@ -15,7 +15,7 @@ extern "C"
 #include "udp.h"
 #include "ipc.h"
 
-#define CHIPVPN_VERSION 300014
+#define CHIPVPN_VERSION 300015
 #define CHIPVPN_PROTOCOL_VERSION 197
 
 #define CHIPVPN_PEER_TIMEOUT 15000
